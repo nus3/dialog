53273e7c3b2955843bde8b5705c9b4cf3ccf6c68
nus3 <hadayota33@gmail.com>
2023-03-23 15:26:36 +0900

    :+1: add dialog1

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const isDialogElement = (target: unknown): target is HTMLDialogElement => {
+  return target instanceof HTMLDialogElement
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const main = () => {
+  const dialog1Button = document.getElementById('dialog1Button')
+  const dialog1 = document.getElementById('dialog1')
+
+  if (!isDialogElement(dialog1)) return
+
+  dialog1Button?.addEventListener('click', () => {
+    dialog1.showModal()
+  })
+}
+
+main()
