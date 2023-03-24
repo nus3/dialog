862221eb71e51e4f4961375c987cc8a691c06925
nus3 <hadayota33@gmail.com>
2023-03-24 15:35:06 +0900

    :+1: fix close animation in safari

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ const isDialogElement = (target: unknown): target is HTMLDialogElement => {
   return target instanceof HTMLDialogElement
 }
 
-// const waitDialogAnimation = (dialog: HTMLDialogElement) =>
-//   Promise.allSettled(
-//     dialog.getAnimations().map((animation) => animation.finished),
-//   )
+const waitDialogAnimation = (dialog: HTMLDialogElement) =>
+  Promise.allSettled(
+    dialog.getAnimations().map((animation) => animation.finished),
+  )
 
 const main = async () => {
   const dialog1Button = document.getElementById('dialog1Button')
@@ -16,6 +16,7 @@ const main = async () => {
   if (!isDialogElement(dialog1)) return
 
   dialog1Button?.addEventListener('click', () => {
+    dialog1.removeAttribute('style')
     dialog1.showModal()
   })
 
@@ -23,11 +24,31 @@ const main = async () => {
   dialog1Close?.addEventListener('click', () => {
     dialog1.close()
   })
+
+  dialog1.addEventListener('close', async (e) => {
+    if (isDialogElement(e.target)) {
+      await waitDialogAnimation(e.target)
+      dialog1.style.display = 'none'
+    }
+  })
+
+  const dialog2Button = document.getElementById('dialog2Button')
+  const dialog2 = document.getElementById('dialog2')
+
+  if (!isDialogElement(dialog2)) return
+
+  dialog2Button?.addEventListener('click', () => {
+    dialog2.showModal()
+  })
+
+  const dialog2Close = document.getElementById('dialog2Close')
+  dialog2Close?.addEventListener('click', () => {
+    dialog2.close()
+  })
 }
 
 main()
 
 // 残りやること
-// スクリーンリーダで読み込まれないようにinertをつける
 // 背景がスクロールしないように
 // フォームがあるダイアログ
